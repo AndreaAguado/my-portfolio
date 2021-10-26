fd4e764ff748888874ac40ea24d9cf4df191ab68
AndreaAguado <andrea95am@gmail.com>
2021-10-26 23:03:11 +0200

    change contents to section landing_page

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import '../styles/App.css';
 function App() {
   return (
     <>
+      <section className="landing_page">
+        <h1>Andrea Aguado Moleón</h1>
+      </section>
       <header className="header"></header>
       <main className="main">
         <h1>Andrea Aguado Moleón</h1>
