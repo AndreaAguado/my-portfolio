7735bedae996dc11fe4e92f3e39042bc6132eca0
AndreaAguado <andrea95am@gmail.com>
2021-10-27 16:59:33 +0200

    add links to footer

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,15 @@ function App() {
           </ul>
         </section>
       </main>
-      <footer className="footer"></footer>
+      <footer className="footer">
+        <nav>
+          <ul>
+            <li><a href="." target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a></li>
+            <li><a href="." target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a></li>
+            <li><a href="." target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a></li>
+          </ul>
+        </nav>
+      </footer>
       {/* </Route> */}
       {/* </Switch> */}
     </>
