fc58fa79272c7d2e967d92fe0a7a7c13a535001c
AndreaAguado <andrea95am@gmail.com>
2021-10-27 20:16:08 +0200

    add styles in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
+import '../styles/layout/Footer.scss';
 const Footer = () => {
     return (
         <footer className="footer">
-            <nav>
-                <ul>
-                    <li><a href="." target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a></li>
-                    <li><a href="." target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a></li>
-                    <li><a href="." target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a></li>
+            <nav className="footer_nav">
+                <ul className="footer_links_list">
+                    <li><a className="footer_link" href="https://twitter.com/andrea114am" target="_blank" rel="noreferrer" title="twitter.com/andrea114am"><i className="fab fa-twitter"></i></a></li>
+                    <li><a className="footer_link" href="https://www.linkedin.com/in/andrea-aguado-mole%C3%B3n/" target="_blank" rel="noreferrer" title="linkedin.com/in/andrea-aguado-moleón/"><i className="fab fa-linkedin-in"></i></a></li>
+                    <li><a className="footer_link" href="https://github.com/AndreaAguado" target="_blank" rel="noreferrer" title="github.com/AndreaAguado"><i class="fab fa-github"></i></a></li>
                 </ul>
             </nav>
+            <small className="footer_small">&copy; Andrea Aguado 2021</small>
         </footer>
     )
 }
