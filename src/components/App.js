da6b9c018dce42680e26b6f85c0f05bba794df5c
AndreaAguado <andrea95am@gmail.com>
2021-10-27 19:40:34 +0200

    add styles to header

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import '../styles/App.scss';
 import Footer from './Footer';
 import Header from './Header';
 import Landing from './Landing';
-// import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
 import projects from '../data/data.json';
 
@@ -29,27 +29,29 @@ function App() {
 
   return (
     <>
-      {/* <Switch> */}
-      {/* <Route exact path="/"> */}
-      <Landing></Landing>
-      {/* </Route> */}
-      {/* <Route path="/home"> */}
-      <Header></Header>
-      <main className="main">
-        <section id="about_me" className="about_me">
-          <h2>Sobre mi</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perferendis ullam officiis dolorum quas, reprehenderit distinctio explicabo labore itaque qui, saepe sequi aperiam. Reiciendis eos repellendus ad dolore vitae omnis.</p>
-        </section>
-        <section id="projects" className="projects">
-          <h2>Proyectos</h2>
-          <ul>
-            {renderProjects()}
-          </ul>
-        </section>
-      </main>
-      <Footer></Footer>
-      {/* </Route> */}
-      {/* </Switch> */}
+      <Switch>
+        <Route exact path="/">
+          <Landing></Landing>
+        </Route>
+        <Route path="/home">
+          <section className="homepage">
+            <Header></Header>
+            <main className="main">
+              <section id="about_me" className="about_me">
+                <h2>Sobre mi</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perferendis ullam officiis dolorum quas, reprehenderit distinctio explicabo labore itaque qui, saepe sequi aperiam. Reiciendis eos repellendus ad dolore vitae omnis.</p>
+              </section>
+              <section id="projects" className="projects">
+                <h2>Proyectos</h2>
+                <ul>
+                  {renderProjects()}
+                </ul>
+              </section>
+            </main>
+            <Footer></Footer>
+          </section>
+        </Route>
+      </Switch>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
+import '../styles/layout/Header.scss';
 const Header = () => {
     return (
         <header className="header">
             <nav>
-                <ul>
-                    <li><a href="#main">Sobre mi</a></li>
-                    <li><a href="#about_me">Proyectos</a></li>
-                    <li><a href=".">Contacto</a></li>
-                    <li><a href=".">CV PDF</a></li>
+                <ul className="header_links_list">
+                    <li><a className="header_link" href="#main">Sobre mi</a></li>
+                    <li><a className="header_link" href="#about_me">Proyectos</a></li>
+                    <li><a className="header_link" href=".">Contacto</a></li>
+                    <li><a className="header_link" href=".">CV PDF</a></li>
                 </ul>
             </nav>
         </header>
