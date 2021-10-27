e067daf48bfe40becd68e65033e46278b75b58b6
AndreaAguado <andrea95am@gmail.com>
2021-10-27 18:28:16 +0200

    add button to landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,9 @@ import '../styles/layout/Landing.scss';
 const Landing = () => {
     return (
         <section className="landing_page">
-            <h1>Andrea Aguado Moleón</h1>
+            <h1>Hola, soy Andrea Aguado Moleón!</h1>
+            {/* <p>Te doy la bienvenida a mi porfolio</p> */}
+            <a className="landing_page_button" href="#about_me">Conóceme</a>
         </section>
     )
 }
