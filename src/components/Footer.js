602c473d77584ab45d4a3319e210fc21d31af741
AndreaAguado <andrea95am@gmail.com>
2021-10-28 20:29:55 +0200

    change class for className in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
                 <ul className="footer_links_list">
                     <li><a className="footer_link" href="https://twitter.com/andrea114am" target="_blank" rel="noreferrer" title="twitter.com/andrea114am"><i className="fab fa-twitter"></i></a></li>
                     <li><a className="footer_link" href="https://www.linkedin.com/in/andrea-aguado-mole%C3%B3n/" target="_blank" rel="noreferrer" title="linkedin.com/in/andrea-aguado-moleón/"><i className="fab fa-linkedin-in"></i></a></li>
-                    <li><a className="footer_link" href="https://github.com/AndreaAguado" target="_blank" rel="noreferrer" title="github.com/AndreaAguado"><i class="fab fa-github"></i></a></li>
+                    <li><a className="footer_link" href="https://github.com/AndreaAguado" target="_blank" rel="noreferrer" title="github.com/AndreaAguado"><i className="fab fa-github"></i></a></li>
                 </ul>
             </nav>
             <small className="footer_small">&copy; Andrea Aguado 2021</small>
