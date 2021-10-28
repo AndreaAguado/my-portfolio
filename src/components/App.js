9d829e17a4355a4cf75682a0d51014b751a68776
AndreaAguado <andrea95am@gmail.com>
2021-10-28 21:43:10 +0200

    clean comments

--- src/components/App.js
+++ src/components/App.js
@@ -31,18 +31,6 @@ function App() {
         <Route path="/home">
           <section className="homepage">
             <Header></Header>
-            {/* <main className="main">
-              <section id="about_me" className="about_me">
-                <h2>Sobre mi</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perferendis ullam officiis dolorum quas, reprehenderit distinctio explicabo labore itaque qui, saepe sequi aperiam. Reiciendis eos repellendus ad dolore vitae omnis.</p>
-              </section>
-              <section id="projects" className="projects">
-                <h2>Proyectos</h2>
-                <ul>
-                  {renderProjects()}
-                </ul>
-              </section>
-            </main> */}
             <Main renderProjects={renderProjects}></Main>
             <Footer></Footer>
           </section>
