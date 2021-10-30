7882d38ed21d1913574cc81823b4ae6c673fb5aa
AndreaAguado <andrea95am@gmail.com>
2021-10-30 22:07:19 +0200

    give styles to paragraph

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,11 +13,11 @@ const Main = (props) => {
     return (
         <main className="main">
             <section id="about_me" className="about_me">
-                <h2>Sobre mi</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perferendis ullam officiis dolorum quas, reprehenderit distinctio explicabo labore itaque qui, saepe sequi aperiam. Reiciendis eos repellendus ad dolore vitae omnis.</p>
+                <h2 className="about_me_title">Sobre mi</h2>
+                <p className="about_me_content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perferendis ullam officiis dolorum quas, reprehenderit distinctio explicabo labore itaque qui, saepe sequi aperiam. Reiciendis eos repellendus ad dolore vitae omnis.</p>
             </section>
             <section id="projects" className="projects">
-                <h2>Proyectos</h2>
+                <h2 className="projects_title">Proyectos</h2>
                 <ul className="projects_list">
                     {renderProjects()}
                 </ul>
