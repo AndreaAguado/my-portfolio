3c3353c10ba0326e29b340264a3c312a3aa4499e
AndreaAguado <andrea95am@gmail.com>
2021-10-30 22:36:33 +0200

    add sparkles to button

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom';
 import '../styles/layout/Landing.scss';
+import Sparkle from 'react-sparkle';
 
 const Landing = () => {
     return (
         <section className="landing_page">
             <h1>Hola, soy Andrea Aguado Moleón!</h1>
             {/* <p>Te doy la bienvenida a mi porfolio</p> */}
-            <Link to="/home#about_me" className="landing_page_button" >Conóceme</Link>
+
+            <div className="button_sparkles">
+                <Link to="/home#about_me" className="landing_page_button" >Conóceme</Link>
+                <Sparkle color={'random'}></Sparkle>
+            </div>
         </section>
     )
 }
