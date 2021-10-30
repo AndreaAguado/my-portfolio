6db354aa440f061688591216380e65991fdf1fcb
AndreaAguado <andrea95am@gmail.com>
2021-10-30 21:03:04 +0200

    add HashLink to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 import '../styles/layout/Header.scss';
+import { HashLink } from 'react-router-hash-link';
 const Header = () => {
     return (
         <header className="header">
             <nav>
                 <ul className="header_links_list">
-                    <li><a className="header_link" href="#main">Sobre mi</a></li>
-                    <li><a className="header_link" href="#about_me">Proyectos</a></li>
-                    <li><a className="header_link" href=".">Contacto</a></li>
-                    <li><a className="header_link" href=".">CV PDF</a></li>
+                    <li>
+                        <HashLink className="header_link" to="/home#about_me">Sobre mi</HashLink>
+
+                    </li>
+                    <li>
+                        <HashLink className="header_link" to="/home#projects">Proyectos</HashLink>
+                    </li>
+                    <li>
+                        <HashLink className="header_link" to="/home#projects">Contacto</HashLink>
+                    </li>
+                    <li>
+                        <HashLink className="header_link" to="/home#projects">CV PDF</HashLink>
+                    </li>
                 </ul>
             </nav>
         </header>
