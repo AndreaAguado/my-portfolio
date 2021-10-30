3dcc53e5faa116e04e29575dd9bb5673f1546062
AndreaAguado <andrea95am@gmail.com>
2021-10-30 23:01:09 +0200

    change sparkles color

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,26 @@
 import { Link } from 'react-router-dom';
 import '../styles/layout/Landing.scss';
 import Sparkle from 'react-sparkle';
+import { useState } from 'react';
 
 const Landing = () => {
+
+    const [hover, SetHover] = useState(false);
+
     return (
         <section className="landing_page">
             <h1>Hola, soy Andrea Aguado Moleón!</h1>
             {/* <p>Te doy la bienvenida a mi porfolio</p> */}
 
-            <div className="button_sparkles">
+            <div onMouseEnter={() => SetHover(true)}
+                onMouseLeave={() => SetHover(false)} className="button_sparkles">
                 <Link to="/home#about_me" className="landing_page_button" >Conóceme</Link>
-                <Sparkle color={'random'}></Sparkle>
+                <Sparkle
+                    className="sparkles"
+                    color={`${hover ? 'red' : 'transparent'}`}>
+                </Sparkle>
             </div>
-        </section>
+        </section >
     )
 }
 export default Landing;
