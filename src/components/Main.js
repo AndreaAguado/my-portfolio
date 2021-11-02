8c8c39306f411a229b92df9c73e5cb13ba64a92c
AndreaAguado <andrea95am@gmail.com>
2021-11-02 22:37:52 +0100

    import react-pdf (in progress)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,11 @@
 import '../styles/layout/Main.scss';
 import ProjectCard from './ProjectCard';
+import { Document, Page } from 'react-pdf';
+import { useState } from 'react';
+import PDF from "../data/AndreaAguadoCVEspañol.pdf";
 const Main = (props) => {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
     const renderProjects = () => {
         return props.data.map((project, index) => {
             return (
@@ -9,6 +14,9 @@ const Main = (props) => {
                 </li>
             )
         })
+    }
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
     }
     return (
         <main className="main">
@@ -49,6 +57,10 @@ const Main = (props) => {
             </section>
             <section id="cv_pdf" className="cv_pdf">
                 <h2 className="cv_pdf_title">CV en PDF</h2>
+                <Document file={PDF} onLoadSuccess={onDocumentLoadSuccess}>
+                    <Page pageNumber={pageNumber} />
+                </Document>
+                <p>Page {pageNumber} of {numPages}</p>
             </section>
         </main>
     )
