ebaa849cbe73efb8e7397d9728e9c8437bacc4a4
AndreaAguado <andrea95am@gmail.com>
2021-11-04 21:54:17 +0100

    add paragraph info

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,7 +22,10 @@ const Main = (props) => {
         <main className="main">
             <section id="about_me" className="about_me">
                 <h2 className="about_me_title">Sobre mi</h2>
-                <p className="about_me_content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perferendis ullam officiis dolorum quas, reprehenderit distinctio explicabo labore itaque qui, saepe sequi aperiam. Reiciendis eos repellendus ad dolore vitae omnis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perferendis ullam officiis dolorum quas, reprehenderit distinctio explicabo labore itaque qui, saepe sequi aperiam. Reiciendis eos repellendus ad dolore vitae omnis.</p>
+                <p className="about_me_content">
+                    Hola!! <span role="img" aria-label="emoji saludo" title="emoji saludo">&#128075;</span> Yo soy Andrea Aguado y soy Front-end Developer Junior e Ingeniera Biomédica. Escogí mi Carrera de ingeniería porque aplicaba la tecnología en ayudar a la medicina. Dada la naturaleza multidisciplinar de la carrera, cuando terminé quise seguir formándome, haciendo el máster de ingeniería biomédica y fue allí donde tuve mi primer acercamiento con el desarrollo web. Tras finalizar mi última formación como desarrolladora web he descubierto una pasión en esta <span role="img" aria-label="emoji corazón" title="emoji corazón">&#10084;&#65039;</span>.
+                    Soy una persona que se enfrenta a los retos con ganas y esforzándome hasta que cumplo todos mis objetivos. Me gusta aprender cosas nuevas y superar mis límites. Soy metódica y cuido mucho los detalles. Aspiro a encontrar el trabajo que me permita seguir creciendo como profesional del desarrollo web <span role="img" aria-label="emoji sonreir" title="emoji sonreir">&#128522;</span> .
+                </p>
             </section>
             <section id="projects" className="projects">
                 <h2 className="projects_title">Proyectos</h2>
