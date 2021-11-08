b380e80116609e832b26a1ba93dd0030c8c836a9
AndreaAguado <andrea95am@gmail.com>
2021-11-08 20:40:35 +0100

    add AboutMe component plus scss

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import '../styles/layout/Main.scss';
+import AboutMe from './AboutMe';
 import ProjectCard from './ProjectCard';
 // import { Document, Page } from 'react-pdf';
 // import { useState } from 'react';
@@ -20,13 +21,7 @@ const Main = (props) => {
     // }
     return (
         <main className="main">
-            <section id="about_me" className="about_me">
-                <h2 className="about_me_title">Sobre mi</h2>
-                <p className="about_me_content">
-                    Hola!! <span role="img" aria-label="emoji saludo" title="emoji saludo">&#128075;</span> Yo soy Andrea Aguado y soy Front-end Developer Junior e Ingeniera Biomédica. Escogí mi Carrera de ingeniería porque aplicaba la tecnología en ayudar a la medicina. Dada la naturaleza multidisciplinar de la carrera, cuando terminé quise seguir formándome, haciendo el máster de ingeniería biomédica y fue allí donde tuve mi primer acercamiento con el desarrollo web. Tras finalizar mi última formación como desarrolladora web he descubierto una pasión en esta <span role="img" aria-label="emoji corazón" title="emoji corazón">&#10084;&#65039;</span>.
-                    Soy una persona que se enfrenta a los retos con ganas y esforzándome hasta que cumplo todos mis objetivos. Me gusta aprender cosas nuevas y superar mis límites. Soy metódica y cuido mucho los detalles. Aspiro a encontrar el trabajo que me permita seguir creciendo como profesional del desarrollo web <span role="img" aria-label="emoji sonreir" title="emoji sonreir">&#128522;</span> .
-                </p>
-            </section>
+            <AboutMe></AboutMe>
             <section id="projects" className="projects">
                 <h2 className="projects_title">Proyectos</h2>
                 <ul className="projects_list">
