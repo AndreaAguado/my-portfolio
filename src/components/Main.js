65a618348715115bc42773463b7d9be9fa213816
AndreaAguado <andrea95am@gmail.com>
2021-11-08 20:33:49 +0100

    add cv

--- src/components/Main.js
+++ src/components/Main.js
@@ -75,6 +75,9 @@ const Main = (props) => {
             </section>
             <section id="cv_pdf" className="cv_pdf">
                 <h2 className="cv_pdf_title">CV en PDF</h2>
+                <a href="https://drive.google.com/drive/folders/1sfdDxitw0VP0ki2lvhRHEt5J0osik0eQ?usp=sharing" target="_blank" rel='noreferrer' title="CV">
+                    <i class='bx bxs-file-pdf'></i>
+                </a>
                 {/* <Document file={PDF} onLoadSuccess={onDocumentLoadSuccess}>
                     <Page pageNumber={pageNumber} />
                 </Document>
