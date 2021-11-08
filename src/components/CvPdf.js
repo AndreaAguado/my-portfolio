a24bb57bb69aef12eaa6e5364ee79d34a5a3f382
AndreaAguado <andrea95am@gmail.com>
2021-11-08 21:17:05 +0100

    add styles to pdf icon

--- src/components/CvPdf.js
+++ src/components/CvPdf.js
@@ -4,8 +4,8 @@ const CvPdf = () => {
     return (
         <section id="cv_pdf" className="cv_pdf">
             <h2 className="cv_pdf_title">CV en PDF</h2>
-            <a href="https://drive.google.com/drive/folders/1sfdDxitw0VP0ki2lvhRHEt5J0osik0eQ?usp=sharing" target="_blank" rel='noreferrer' title="CV">
-                <i class='bx bxs-file-pdf'></i>
+            <a href="https://drive.google.com/drive/folders/1sfdDxitw0VP0ki2lvhRHEt5J0osik0eQ?usp=sharing" target="_blank" rel='noreferrer' title="Ir a CV en PDF" className="cv_pdf_link">
+                <i class='bx bxs-file-pdf cv_pdf_icon'></i>
             </a>
         </section>
     )
