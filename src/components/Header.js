8b3a807062ce07361660fb77df48b861834a23ed
AndreaAguado <andrea95am@gmail.com>
2021-11-10 11:58:59 +0100

    fix import of useState

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import '../styles/layout/Header.scss';
 import { HashLink } from 'react-router-hash-link';
-import { useState } from 'react/cjs/react.development';
+// import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 
 const Header = () => {
 
