9ad7f92988c189061718b3b9fe03a2081585ee47
AndreaAguado <andrea95am@gmail.com>
2021-12-14 17:56:07 +0100

    delete second header

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,31 +15,10 @@ const Header = () => {
     }
     return (
         <header className="header">
-            <nav className="header_nav_big">
+            <nav className={collapsed ? "header_nav_big hidden" : "header_nav_big"}>
                 <ul className="header_links_list">
                     <li>
                         <HashLink className="header_link" to="/home#about_me">Sobre mi</HashLink>
-
-                    </li>
-                    <li>
-                        <HashLink className="header_link" to="/home#projects">Proyectos</HashLink>
-                    </li>
-                    <li>
-                        <HashLink className="header_link" to="/home#tools">Tecnologías</HashLink>
-                    </li>
-                    <li>
-                        <HashLink className="header_link" to="/home#contact">Contacto</HashLink>
-                    </li>
-                    <li>
-                        <HashLink className="header_link" to="/home#cv_pdf">CV PDF</HashLink>
-                    </li>
-                </ul>
-            </nav>
-            <nav className={collapsed ? "header_nav_small hidden" : "header_nav_small"}>
-                <ul className="header_links_list">
-                    <li>
-                        <HashLink className="header_link" to="/home#about_me">Sobre mi</HashLink>
-
                     </li>
                     <li>
                         <HashLink className="header_link" to="/home#projects">Proyectos</HashLink>
@@ -56,7 +35,6 @@ const Header = () => {
                 </ul>
             </nav>
             <i onClick={toggleMenu} className="fas fa-bars burger_menu"></i>
-
         </header>
     )
 }
